7549baca3eb729b43606df886473bbc502359f04
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-04-02 19:38:24 +0530

    decision tree regression
    the dataset is same as polynomial regression. it is better if done with multidimensions.

---
+++ decision_tree_regression_suniti.py
@@ -0,0 +1,35 @@
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+from sklearn.tree import DecisionTreeRegressor
+reg=  DecisionTreeRegressor(random_state= 0 )
+reg.fit(X,y)
+
+y_pred = regressor.predict([[6.5]])
+
+# Visualising the Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, reg.predictt(X), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, reg.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
