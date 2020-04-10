c65380b254fd96cd40d26ad33407efed8c194772
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-04-10 14:06:54 +0530

    Delete delhi-air-pollution.py

--- delhi-air-pollution.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df1= pd.read_excel('delhi air 1996 to 2015.xls')
-df2= pd.read_excel('delhi weather since 1997.xlsx') 
-
-df3= pd.merge(df1, df2, how='inner', on=['month','year'])
-df3.fillna(df3.mean())
