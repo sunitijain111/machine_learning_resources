e027290f4066ac9adbfcfe17320b438b157c9747
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-05-08 20:12:59 +0530

    Add files via upload

--- decision_tree_regression_suniti.py
+++ decision_tree_regression_suniti.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import metrics
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -13,16 +14,20 @@ y = dataset.iloc[:, 2].values
 from sklearn.tree import DecisionTreeRegressor
 reg=  DecisionTreeRegressor(random_state= 0 )
 reg.fit(X,y)
+y_pred= reg.predict(X)
+print("r- squared: ", metrics.r2_score( y,y_pred ) )
 
 y_pred = reg.predict([[6.5]])
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
-plt.plot(X, reg.predictt(X), color = 'blue')
+plt.plot(X, reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
+
+
 
 # Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
